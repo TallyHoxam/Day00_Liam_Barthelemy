01e9db129b80af5d28cc9a5971f427b78c8de3f1
Kyoshiro26 <122786345+ZywiiZ@users.noreply.github.com>
2024-05-13 13:51:01 +0200

    Update ex04.py

--- ex04/ex04.py
+++ ex04/ex04.py
@@ -3,6 +3,13 @@ def tableau():
     tableau_un = []
     for i in range(15):
         tableau_un.append(random.random())
-        print(tableau_un)
+    print(tableau_un)
+    tableau_croissant = []
+    tableau_croissant.append[tableau_un[0])
+    for carac in tableau_un :
+        if carac > tableau_croissant[0]
+        
+
+        
         
 tableau()
